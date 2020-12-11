adbe4d7f79fa45084f5cede9b35bfd357b33e371
Myu-Kato <s19b701@shohoku.ac.jp>
2020-12-11 10:35:52 +0900

    //歩数計test //2020/11/20 //MyuKato

--- app/src/main/java/jp/ac/shohoku/s19b703/shibuyakai/GameActivity.java
+++ app/src/main/java/jp/ac/shohoku/s19b703/shibuyakai/GameActivity.java
@@ -2,10 +2,15 @@ package jp.ac.shohoku.s19b703.shibuyakai;
 
 //ゲームメイン画面
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
@@ -18,7 +23,29 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
         moveExpo();
         moveCount();
+        displayCharacter();
     }
+
+    private void displayCharacter(){
+        SharedPreferences stepData = GameActivity.this.getSharedPreferences("stepData", Context.MODE_PRIVATE);
+        int total = stepData.getInt("step",0);
+        ImageView character = findViewById(R.id.characterView);
+
+        total = 100;
+
+        if(total < 100){
+            character.setImageResource(R.drawable.case1);
+        }else if(total < 200){
+            character.setImageResource(R.drawable.s2);
+        }else if(total < 300){
+            character.setImageResource(R.drawable.s3);
+        }else if(total < 400){
+            character.setImageResource(R.drawable.s4);
+        }else{
+            character.setImageResource(R.drawable.s5);
+        }
+    }
+
     private void moveCount() {
         Button count = findViewById(R.id.countButton);
         count.setOnClickListener(new View.OnClickListener() {
